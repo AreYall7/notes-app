1f364313fac48f05cfbcb7619a8cc5d394817f23
AreYall7 <ryall.edwards7@gmail.com>
2020-09-03 15:00:03 -0500

    Updates

---
+++ precache-manifest.281238348576228bc07ed31ab46873d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e84120b877702993d2c3949cee6480",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "55ea24fe9aab805eefd5",
+    "url": "/notes-app/static/css/main.8a7b1b22.chunk.css"
+  },
+  {
+    "revision": "e75057c2134222f0540f",
+    "url": "/notes-app/static/js/2.eb7291ed.chunk.js"
+  },
+  {
+    "revision": "c0c0a28e3e1b198d368782bdda7f2849",
+    "url": "/notes-app/static/js/2.eb7291ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ea24fe9aab805eefd5",
+    "url": "/notes-app/static/js/main.477d7b15.chunk.js"
+  },
+  {
+    "revision": "62aad0d4a74a01805c42",
+    "url": "/notes-app/static/js/runtime-main.9a7419d2.js"
+  }
+]);
